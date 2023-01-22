d2cb16962724e225cd9719d3196797f6333bbacc
parag-b <72294154+parag-b@users.noreply.github.com>
2023-01-22 20:07:15 +0530

    zerodha login/auth

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/parag-b/zerodha-api
+
+go 1.19
+
+require (
+	github.com/asmcos/requests v0.0.0-20210319030608-c839e8ae4946
+	github.com/pquerna/otp v1.4.0
+	github.com/zerodha/gokiteconnect/v4 v4.1.0
+)
+
+require (
+	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
+	github.com/gocarina/gocsv v0.0.0-20180809181117-b8c38cb1ba36 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+)
---
+++ login.go
@@ -0,0 +1,171 @@
+package zerodhaapi
+
+import (
+	"errors"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/asmcos/requests"
+	totp "github.com/pquerna/otp/totp"
+	kiteconnect "github.com/zerodha/gokiteconnect/v4"
+)
+
+const (
+	kiteLoginUrl     = "https://kite.zerodha.com/api/login"
+	twofactorAuthUrl = "https://kite.zerodha.com/api/twofa"
+	reqTokenUrl      = "https://kite.zerodha.com/connect/login?v=3&api_key="
+)
+
+type ZerodhaApi struct {
+	UserId    string
+	Password  string
+	ApiKey    string
+	ApiSecret string
+	TotpKey   string
+
+	KiteConn     *kiteconnect.Client
+	IsKiteAuth   bool
+	KiteReqId    string
+	KiteReqToken string
+}
+
+func New(za *ZerodhaApi) error {
+
+	za.IsKiteAuth = false
+	req := requests.Requests()
+
+	if err := za.doCredentialAuth(req); err != nil {
+		return err
+	}
+
+	if err := za.doTwoFactorAuth(req); err != nil {
+		return err
+	}
+
+	if err := za.getReqToken(req); err != nil {
+		return err
+	}
+
+	za.KiteConn = kiteconnect.New(za.ApiKey)
+	data, err := za.KiteConn.GenerateSession(za.KiteReqToken, za.ApiSecret)
+	if err != nil {
+		return errors.New("invalid api secret")
+	}
+
+	za.KiteConn.SetAccessToken(data.AccessToken)
+
+	za.IsKiteAuth = true
+
+	return nil
+}
+
+func (z *ZerodhaApi) doCredentialAuth(req *requests.Request) error {
+
+	data := requests.Datas{
+		"user_id":  z.UserId,
+		"password": z.Password,
+	}
+
+	resp, err := req.Post(kiteLoginUrl, data)
+
+	print(resp)
+
+	if (err != nil) || (resp.R.StatusCode != 200) {
+		return errors.New("invalid credentials")
+	}
+
+	z.KiteReqId = extractValue(resp.Text(), "request_id")
+
+	return nil
+}
+
+func (z *ZerodhaApi) doTwoFactorAuth(req *requests.Request) error {
+
+	totpCode, err := totp.GenerateCode(z.TotpKey, time.Now())
+
+	if err != nil {
+		return errors.New("invalid two factor authentication")
+	}
+
+	data := requests.Datas{
+		"user_id":     z.UserId,
+		"request_id":  z.KiteReqId,
+		"twofa_value": totpCode,
+	}
+
+	resp, err := req.Post(twofactorAuthUrl, data)
+
+	if (err != nil) || (resp.R.StatusCode != 200) {
+		return errors.New("invalid two factor authentication")
+	}
+	return nil
+}
+
+func (z *ZerodhaApi) getReqToken(req *requests.Request) error {
+
+	req.SetTimeout(5)
+	resp, err := req.Get(reqTokenUrl + z.ApiKey)
+
+	if err != nil {
+		// Not able to reach http handler, parse the URL for key
+		arr := strings.Split(err.Error(), `"`) // split on '&'
+		val, key := extractKeyValue(arr[1], "request_token")
+
+		if key {
+			z.KiteReqToken = val
+			return nil
+		}
+	} else {
+		// Able to reach http handler, parse the response
+		if resp.R.StatusCode != 200 {
+			return errors.New("invalid api key")
+		}
+
+		m, _ := url.ParseQuery(resp.R.Request.URL.RawQuery)
+		if _, ok := m["request_token"]; ok {
+			z.KiteReqToken = m["request_token"][0]
+			return nil
+		}
+
+	}
+
+	return errors.New("invalid api key")
+}
+
+func (z ZerodhaApi) UserMargins() (kiteconnect.AllMargins, error) {
+	return z.KiteConn.GetUserMargins()
+}
+
+func (z ZerodhaApi) CashBalance() (float64, error) {
+
+	if !z.IsKiteAuth {
+		return 0, errors.New("account not authenticated")
+	}
+	margins, err := z.KiteConn.GetUserMargins()
+	if err != nil {
+		return 0, err
+	}
+	return margins.Equity.Net, nil
+}
+
+func extractValue(body string, key string) string {
+	keystr := "\"" + key + "\":[^,;\\]}]*"
+	r, _ := regexp.Compile(keystr)
+	match := r.FindString(body)
+	keyValMatch := strings.Split(match, ":")
+	return strings.ReplaceAll(keyValMatch[1], "\"", "")
+}
+
+func extractKeyValue(body string, key string) (string, bool) {
+	arr := strings.Split(body, `&`) // split on '&'
+
+	for index := range arr {
+		if strings.Contains(arr[index], key) { // if key is found
+			arrVal := strings.Split(arr[index], `=`)
+			return arrVal[1], true // Extract value
+		}
+	}
+	return "", false
+}
